cce16facdb1cb504228a29a1f3d3cd8a69f001ba
Cristhian <twisticdigital@gmail.com>
2025-04-10 12:25:19 +0100

    refactor: comment out unused navigation components and clean up template

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,16 @@
 <script setup lang="js">
 import { RouterLink, RouterView } from 'vue-router'
-import NavUnlog from './Navs/NavUnlog.vue'
-import NavAdmin from './Navs/NavAdmin.vue'
-import NavClient from './Navs/NavClient.vue'
+//import NavUnlog from './Navs/NavUnlog.vue'
+//import NavAdmin from './Navs/NavAdmin.vue'
+//import NavClient from './Navs/NavClient.vue'
 import './styles.css'
 import { ref } from 'vue'
+import { useAbility } from '@casl/vue'
 </script>
-
-<template>
-  <div v-if="role === 'unlogged'">
-    <NavUnlog/>
-  </div>
-  <div v-else-if="role === 'admin'">
-    <NavAdmin/>
-  </div>
-  <div v-else-if="role === 'client'">
-    <NavClient/>
-  </div>
-  <div v-else>
-    <p>Hubo un problema con la selección de página</p>
-  </div>
-</template>
 
 
 <script lang="js">
 
     const role = 'client';
 
-</script> 
+</script>
