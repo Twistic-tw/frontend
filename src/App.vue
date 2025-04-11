5f4ec59a5580486cb91bc7771252fd424644f21a
Cristhian <twisticdigital@gmail.com>
2025-04-11 10:12:32 +0100

    Refactor App.vue to use TypeScript and enhance navigation based on user login status

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
-<script setup lang="js">
-import { RouterView } from 'vue-router'
-import NavUnlog from './Navs/NavUnlog.vue'
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+import NavUnlog from './Navs/NavUnlog.vue';
+import NavLogged from './Navs/NavLogged.vue'
 import './styles.css'
+
+// Manejar mostrar las barras si está logeado
+import { ref } from 'vue';
+
+const isLogged = ref(false);
+
+// Saber si está logeado
+const userSession = document.cookie.includes('laravel_session') || document.cookie.includes('XSRF-TOKEN');
+isLogged.value = userSession;
 </script>
 
 <template>
   <div class="min-h-screen flex flex-col bg-gradient-to-r from-white via-slate-200 to-slate-400 dark:from-neutral-950 dark:to-slate-900">
 
     <header>
-      <NavUnlog />
+      <NavLogged v-if="isLogged" />
+      <NavUnlog v-else />
     </header>
 
     <!-- Solo contenido principal -->
