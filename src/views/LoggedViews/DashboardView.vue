a674ba099678e57cd7ae24574abd4f4d5cdadb74
Cristhian <twisticdigital@gmail.com>
2025-04-11 13:37:55 +0100

    Fix DashboardView.vue: Correct typo in user management card title

--- src/views/LoggedViews/DashboardView.vue
+++ src/views/LoggedViews/DashboardView.vue
@@ -43,7 +43,7 @@ const userName = sessionStorage.getItem('userName');
 
       <!-- Card 4 -->
       <div v-if="role && (role === 'admin')" class="p-6 bg-white rounded-2xl shadow-md hover:shadow-lg transition dark:bg-gray-800">
-        <h2 class="text-xl font-semibold text-gray-700 dark:text-white mb-4">Gestiona usuarios</h2>
+        <h2 class="text-xl font-semibold text-gray-700 dark:text-white mb-4">Gestionar usuarios</h2>
         <p class="text-gray-500 dark:text-gray-300">Consulta, modifica y elimina usuarios de la plataforma.</p>
         <button class="mt-4 inline-block bg-indigo-600 text-white px-4 py-2 rounded-lg hover:bg-indigo-700">
           Consultar usuarios
