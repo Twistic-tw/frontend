f5af533932ee41ac82b25f7594ad19d2c1ff597c
Cristhian <twisticdigital@gmail.com>
2025-04-11 12:38:07 +0100

    Refactor NavUnlog.vue: Remove unused RouterView import and adjust layout

--- src/Navs/NavUnlog.vue
+++ src/Navs/NavUnlog.vue
@@ -1,23 +1,20 @@
 <script setup lang="ts">
-//import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink } from 'vue-router'
 import '../styles.css'
 //import { ref } from 'vue'
 </script>
 
 <template>
-    <div class="flex h-screen dark:bg-neutral-950">
+    <div class="flex h-auto dark:bg-neutral-950">
         <nav class="flex h-15 items-center bg-gradient-to-l from-neutral-950 to-slate-800 w-full fixed top-0">
             <div class="flex justify-end flex-1">
                 <RouterLink to="/login" class="mx-2 px-10 py-2 text-center text-lg font-semibold tracking-tight rounded-2xl transition duration-200 ease-in-out text-white hover:bg-slate-800" active-class="bg-slate-900">Login</RouterLink>
                 <RouterLink to="/signup" class="mx-2 px-10 py-2 text-center text-lg font-semibold tracking-tight rounded-2xl transition duration-200 ease-in-out text-white hover:bg-slate-800" active-class="bg-slate-900">Signup</RouterLink>
             </div>
         </nav>
-        <RouterLink to="/" class="peer bg-white dark:bg-neutral-950 w-[200px] h-[60px] rounded-tr-3xl flex text-4xl items-center justify-center transition-all duration-200 ease-in-out fixed top-0">
+            <RouterLink to="/" class="peer bg-white dark:bg-neutral-950 w-[200px] h-[60px] rounded-tr-3xl flex text-4xl items-center justify-center transition-all duration-200 ease-in-out fixed top-0">
                 <p class="text-neutral-950 dark:text-white ">TWISTIC</p>
             </RouterLink>
-        <div class="flex grow">
-
-        </div>
     </div>
 </template>
 
