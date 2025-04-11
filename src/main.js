c87fc904073467634c48f75a62cbc99789d23d93
Cristhian <twisticdigital@gmail.com>
2025-04-11 12:12:41 +0100

    Remove commented-out bootstrap function for user authentication in main.js

--- src/main.js
+++ src/main.js
@@ -19,44 +19,6 @@ router.afterEach((to) => {
 });
 
 axios.defaults.withCredentials = true;
-/*
-async function bootstrap() {
-  try {
-    // Consultar al backend el usuario actual
-    const response = await axios.get('https://api-catalogos.twistic.app/api/user', {withCredentials: true});
-
-    // Guarda la respuesta
-    const user = response.data;
-    // Extrae el rol
-    const userRole = user.roles[0];
-
-    // Crear las abilities según su rol
-    const ability = defineAbilitiesFor(userRole);
-
-    // Crear y montar la app
-    const app = createApp(App);
-
-    app.use(createPinia());
-    app.use(router);
-    app.use(PrimeVue);
-    app.use(abilitiesPlugin, ability)
-
-    app.mount('#app');
-  } catch (error) {
-    console.error('Error al obtener el usuario:', error);
-
-    if (error.response && error.response.status === 401) {
-      // Si el usuario no está autenticado, redirigir al login
-      window.location.href = '/';
-    }else {
-      // Manejar otros errores
-      console.error('Error inesperado:', error);
-    }
-  }
-}
-
-bootstrap();
-*/
 
 import App from './App.vue'
 import router from './router'
