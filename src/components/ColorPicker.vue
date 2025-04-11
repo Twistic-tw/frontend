e2c4aa9c2f9fcd911dad4b0e0537e2faa377be6d
Twistic-tw <twisticdigital@gmail.com>
2025-04-11 13:49:48 +0100

    feat: Make PDF view design

---
+++ src/components/ColorPicker.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="flex flex-col">
+        <ColorPicker v-model="colorHEX" inputId="cp-hex" format="hex" class="rounded-2xl" />
+        <span class="font-medium flex-col">{{ colorHEX }}</span>
+    </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import ColorPicker from 'primevue/colorpicker';
+
+const colorHEX = ref('6466f1');
+</script>
