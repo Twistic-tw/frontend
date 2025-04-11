edbf5b11869ce32a79ea1f959872c0229382c5b1
Cristhian <twisticdigital@gmail.com>
2025-04-11 11:29:08 +0100

    Refactor DashboardView.vue to switch script language from JavaScript to TypeScript

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
 import NavUnlog from './Navs/NavUnlog.vue';
-import NavAdmin from './Navs/NavAdmin.vue';
 import './styles.css'
 
 // Manejar mostrar las barras si está logeado
