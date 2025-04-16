1fcecc99e50ecf08236cb04c2d3be45c6811ef32
Cristhian <twisticdigital@gmail.com>
2025-04-16 19:38:53 +0100

    refactor(AllTemplatesView): Consolidate script setup and remove duplicate code

--- src/views/AdminViews/AllTemplatesView.vue
+++ src/views/AdminViews/AllTemplatesView.vue
@@ -1,3 +1,45 @@
+
+<script setup lang="ts">
+import { ref, onMounted, watch } from 'vue'
+import axios from 'axios'
+
+const usuarios = ref([])
+const plantillas = ref([])
+const usuarioSeleccionado = ref('')
+
+// Cargar usuarios al iniciar
+onMounted(async () => {
+  try {
+    const resUsers = await axios.get('https://api-catalogos.twistic.app/api/Users')
+    usuarios.value = resUsers.data
+
+    await cargarPlantillas() // carga inicial con todas las plantillas
+  } catch (error) {
+    console.error('Error al cargar usuarios o plantillas:', error)
+  }
+})
+
+// Cargar plantillas (todas o por usuario)
+async function cargarPlantillas() {
+  try {
+    const url = usuarioSeleccionado.value
+      ? `https://api-catalogos.twistic.app/api/templates?id_user=${usuarioSeleccionado.value}`
+      : 'https://api-catalogos.twistic.app/api/ViewTemplates'
+    const res = await axios.get(url)
+    plantillas.value = res.data
+  } catch (error) {
+    console.error('Error al cargar plantillas:', error)
+    plantillas.value = []
+  }
+}
+
+// Ver cambios en el select y recargar plantillas
+watch(usuarioSeleccionado, () => {
+  cargarPlantillas()
+})
+</script>
+
+
 <template>
   <div class="p-6 bg-white rounded-xl shadow-md  mt-14">
     <h2 class="text-2xl font-bold text-violet-700 mb-4">Gestión de Plantillas por Usuario</h2>
@@ -45,43 +87,3 @@
     </table>
   </div>
 </template>
-
-<script setup lang="ts">
-import { ref, onMounted, watch } from 'vue'
-import axios from 'axios'
-
-const usuarios = ref([])
-const plantillas = ref([])
-const usuarioSeleccionado = ref('')
-
-// Cargar usuarios al iniciar
-onMounted(async () => {
-  try {
-    const resUsers = await axios.get('https://api-catalogos.twistic.app/api/Users')
-    usuarios.value = resUsers.data
-
-    await cargarPlantillas() // carga inicial con todas las plantillas
-  } catch (error) {
-    console.error('Error al cargar usuarios o plantillas:', error)
-  }
-})
-
-// Cargar plantillas (todas o por usuario)
-async function cargarPlantillas() {
-  try {
-    const url = usuarioSeleccionado.value
-      ? `https://api-catalogos.twistic.app/api/templates?id_user=${usuarioSeleccionado.value}`
-      : 'https://api-catalogos.twistic.app/api/ViewTemplates'
-    const res = await axios.get(url)
-    plantillas.value = res.data
-  } catch (error) {
-    console.error('Error al cargar plantillas:', error)
-    plantillas.value = []
-  }
-}
-
-// Ver cambios en el select y recargar plantillas
-watch(usuarioSeleccionado, () => {
-  cargarPlantillas()
-})
-</script>
