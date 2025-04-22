97674cceef998356ca5424755c70e2061bb3328b
Cristhian <twisticdigital@gmail.com>
2025-04-22 15:34:06 +0100

    fix(AllCataloguesView.vue): Add bottom margin to the main container for improved spacing

--- src/views/AdminViews/AllCataloguesView.vue
+++ src/views/AdminViews/AllCataloguesView.vue
@@ -1,6 +1,6 @@
 
 <template>
-    <div class="flex-1 mt-[60px] ml-21 flex-col justify-center px-6 py-12 lg:px-8 ">
+    <div class="flex-1 mt-[60px] ml-21 flex-col justify-center px-6 py-12 lg:px-8 mt-4">
 
     </div>
 </template>
