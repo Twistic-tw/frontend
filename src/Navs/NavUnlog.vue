44b2d1494669b5938a6ec3ca8ad7e76cdca584dd
Cristhian <twisticdigital@gmail.com>
2025-04-22 15:13:47 +0100

    fix(NavUnlog.vue): Add z-index to main container for improved stacking context

--- src/Navs/NavUnlog.vue
+++ src/Navs/NavUnlog.vue
@@ -5,7 +5,7 @@ import '../styles.css'
 </script>
 
 <template>
-    <div class="flex h-auto dark:bg-neutral-950">
+    <div class="flex h-auto dark:bg-neutral-950 z-50">
         <nav class="flex h-15 items-center bg-gradient-to-l from-neutral-950 to-slate-800 w-full fixed top-0">
             <div class="flex justify-end flex-1">
                 <RouterLink to="/login" class="mx-2 px-10 py-2 text-center text-lg font-semibold tracking-tight rounded-2xl transition duration-200 ease-in-out text-white hover:bg-slate-800" active-class="bg-slate-900">Login</RouterLink>
