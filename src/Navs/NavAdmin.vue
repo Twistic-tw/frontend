4696909aec921cc320d66223c72f8c45a1584449
Cristhian <twisticdigital@gmail.com>
2025-04-22 17:48:54 +0100

    fix(NavAdmin.vue): Adjust margin of toggle button for improved layout

--- src/Navs/NavAdmin.vue
+++ src/Navs/NavAdmin.vue
@@ -59,7 +59,7 @@ onBeforeUnmount(() => {
       <!-- Toggle botón -->
       <div
         @click.stop="ToggleNav"
-        class="rounded-tl-2xl rounded-br-2xl bg-gradient-to-r from-indigo-600 to-cyan-500 size-12 mr-15 transition-all duration-300 ease-in-out hover:shadow-[0px_0px_10px_2px_rgba(255,_255,_255,_0.2)] cursor-pointer">
+        class="rounded-tl-2xl rounded-br-2xl bg-gradient-to-r from-indigo-600 to-cyan-500 size-12 mr-6 transition-all duration-300 ease-in-out hover:shadow-[0px_0px_10px_2px_rgba(255,_255,_255,_0.2)] cursor-pointer">
       </div>
     </nav>
 
