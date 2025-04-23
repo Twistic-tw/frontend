9187f9d553edac444fdfd84acc03f1c4691460d7
Cristhian <twisticdigital@gmail.com>
2025-04-23 14:46:53 +0100

    fix(router): add authentication check for admin routes and update meta properties

--- src/router/index.js
+++ src/router/index.js
@@ -16,67 +16,90 @@ const router = createRouter({
       meta: { title: 'Logout' }
     },
     {
-      path: '/',
-      name: 'home',
-      component: () => import('../views/UnlogViews/HomeView.vue'),
-      meta: { title: 'Catálogos | Twistic' }
-    },
-    {
-      path: '/dashboard',
-      name: 'dashboard',
-      component: () => import('../views/LoggedViews/DashboardView.vue'),
-      meta: { title: 'Dashboard' }
-    },
-    {
-      path: '/profile',
-      name: 'profile',
-      component: () => import('../views/LoggedViews/ProfileView.vue'),
-      meta: { title: 'Profile' }
-    },
-    {
-      path: '/pdf',
-      name: 'pdf',
-      component: () => import('../views/UserViews/MakePDFView.vue'),
-      meta: { title: 'PDF' }
-    },
-    {
-      path: '/templates',
-      name: 'templates',
-      component: () => import('../views/AdminViews/AllTemplatesView.vue'),
-      meta: { title: 'Templates' }
-    },
-    {
-      path: '/fields',
-      name: 'fields',
-      component: () => import('../views/AdminViews/AllFieldsView.vue'),
-      meta: { title: 'Fields' }
-    },
-    {
       path: '/signup',
       name: 'signup',
       component: () => import('../views/UnlogViews/SignupView.vue'),
       meta: { title: 'Singup' }
     },
-
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/UnlogViews/HomeView.vue'),
+      meta: { title: 'Catálogos | Twistic'}
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: () => import('../views/LoggedViews/DashboardView.vue'),
+      meta: { title: 'Dashboard' , requiereNavAdmin: true}
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: () => import('../views/LoggedViews/ProfileView.vue'),
+      meta: { title: 'Profile' , requiereNavAdmin: true}
+    },
+    {
+      path: '/pdf',
+      name: 'pdf',
+      component: () => import('../views/UserViews/MakePDFView.vue'),
+      meta: { title: 'PDF' , requiereNavAdmin: true}
+    },
+    {
+      path: '/templates',
+      name: 'templates',
+      component: () => import('../views/AdminViews/AllTemplatesView.vue'),
+      meta: { title: 'Templates' , requiereNavAdmin: true}
+    },
+    {
+      path: '/fields',
+      name: 'fields',
+      component: () => import('../views/AdminViews/AllFieldsView.vue'),
+      meta: { title: 'Fields' , requiereNavAdmin: true}
+    },
     {
       path: '/catalogs',
       name: 'catalogs',
       component: () => import('../views/AdminViews/AllCataloguesView.vue'),
-      meta: { title: 'Catalogs List' }
+      meta: { title: 'Catalogs List' , requiereNavAdmin: true}
     },
     {
       path: '/users',
       name: 'users',
       component: () => import('../views/AdminViews/UsersListView.vue'),
-      meta: { title: 'Users Management' }
+      meta: { title: 'Users Management' , requiereNavAdmin: true}
     },
     {
       path: '/notifications',
       name: 'notifications',
       component: () => import('../views/LoggedViews/NotificationCenter.vue'),
-      meta: { title: 'Notifications' }
+      meta: { title: 'Notifications' , requiereNavAdmin: true}
     }
   ],
 })
+
+// Navigation Guard
+
+router.beforeEach(async (to, from, next) => {
+  const requiresAuth = to.meta.requiereNavAdmin;
+
+  // Verificar si está logueado
+  let isLoggedIn = false;
+
+  try {
+    const res = await fetch('https://api-catalogos.twistic.app/api/user', {
+      credentials: 'include'
+    });
+    isLoggedIn = res.ok;
+  } catch (err) {
+    console.error('Error verificando sesión:', err);
+  }
+
+  if (requiresAuth && !isLoggedIn) {
+    next('/login');
+  } else {
+    next();
+  }
+});
 
 export default router
