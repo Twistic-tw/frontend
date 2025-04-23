89aab169f4e7970730875c96c40318766dd88c33
Cristhian <twisticdigital@gmail.com>
2025-04-23 11:56:29 +0100

    fix(SignupView.vue): update input bindings for name and role fields in the registration form

--- src/views/UnlogViews/SignupView.vue
+++ src/views/UnlogViews/SignupView.vue
@@ -54,14 +54,14 @@ function handleRegister() {
           <div>
             <label for="name" class="block text-sm/6 font-medium text-gray-900 dark:text-indigo-50">Name</label>
             <div class="mt-2">
-              <input v-model="email" type="text" name="name" id="name" autocomplete="name" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 dark:bg-gray-500 dark:text-indigo-50 dark:outline-gray-500 dark:focus:outline-gray-800"/>
+              <input v-model="name" type="text" name="name" id="name" autocomplete="name" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 dark:bg-gray-500 dark:text-indigo-50 dark:outline-gray-500 dark:focus:outline-gray-800"/>
             </div>
           </div>
 
           <div>
             <label for="role" class="block text-sm/6 font-medium text-gray-900 dark:text-indigo-50">Role</label>
             <div class="mt-2">
-              <input v-model="email" type="text" name="role" id="role" autocomplete="role" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 dark:bg-gray-500 dark:text-indigo-50 dark:outline-gray-500 dark:focus:outline-gray-800"/>
+              <input v-model="role" type="text" name="role" id="role" autocomplete="role" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 dark:bg-gray-500 dark:text-indigo-50 dark:outline-gray-500 dark:focus:outline-gray-800"/>
             </div>
           </div>
 
