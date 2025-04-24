e67181855337b0cb44c8f4f3c476871e8ba71981
Cristhian <twisticdigital@gmail.com>
2025-04-24 11:18:06 +0100

    fix(UsersListView): update search input background color for better visibility

--- src/views/AdminViews/UsersListView.vue
+++ src/views/AdminViews/UsersListView.vue
@@ -20,7 +20,7 @@ const {
         v-model="searchQuery"
         type="text"
         placeholder="Search user by name..."
-        class="w-full px-4 py-2 rounded-lg border border-gray-300 dark:border-gray-700 dark:bg-gray-800 dark:text-white focus:outline-none focus:ring-2 focus:ring-indigo-500"
+        class="w-full px-4 py-2 rounded-lg bg-white border border-gray-300 dark:border-gray-700 dark:bg-gray-800 dark:text-white focus:outline-none focus:ring-2 focus:ring-indigo-500"
       />
     </div>
 
