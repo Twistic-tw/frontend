f117faf40ccf79520042fc6c9445bad4bdd14dff
Cristhian <twisticdigital@gmail.com>
2025-04-25 14:40:25 +0100

    fix(NewCatalogView): update draggable component to use v-bind for selected headers list

--- src/views/LoggedViews/NewCatalogView.vue
+++ src/views/LoggedViews/NewCatalogView.vue
@@ -165,7 +165,7 @@ export default {
 
           <div v-if="step === 3" class="bg-white p-6 rounded shadow">
             <h3 class="text-lg font-bold mb-4 text-center">Reorder Fields</h3>
-            <draggable v-model="form.selected_headers" class="bg-gray-50 p-4 rounded shadow space-y-2" item-key="name" animation="200">
+            <draggable :list="form.selected_headers" item-key="name" class="bg-gray-50 p-4 rounded shadow space-y-2" animation="200">
               <template #item="{ element, index }">
                 <div class="p-3 bg-gray-100 rounded cursor-move flex items-center">
                   <span class="font-semibold text-gray-700 mr-2">{{ index + 1 }}.</span>
