36f77bade6b91ba8ff6ec07dc6167687968b624d
Cristhian <twisticdigital@gmail.com>
2025-04-29 13:31:19 +0100

    New Create Template Card

--- src/views/LoggedViews/DashboardView.vue
+++ src/views/LoggedViews/DashboardView.vue
@@ -47,8 +47,17 @@ onMounted(fetchNotifications);
     <!-- Main Cards -->
     <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
 
-      <!-- Create Catalogs Card (client and admin) -->
+      <!-- Create Template Card (client and admin) -->
       <RouterLink to="/newCatalog" v-if="role && (role === 'client' || role === 'admin')" class="p-6 bg-white rounded-2xl shadow-md hover:shadow-lg transition dark:bg-gray-800">
+        <h2 class="text-xl font-semibold text-gray-700 dark:text-white mb-4">Create Template</h2>
+        <p class="text-gray-500 dark:text-gray-300">Create template for your catalog.</p>
+        <button class="mt-4 inline-block bg-[#4f39f6] text-white px-6 py-2 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 hover:bg-[#3a2ac9]">
+          Create Template
+        </button>
+      </RouterLink>
+
+      <!-- Create Catalogs Card (admin) -->
+      <RouterLink to="/newCatalog" v-if="role && (role === 'admin')" class="p-6 bg-white rounded-2xl shadow-md hover:shadow-lg transition dark:bg-gray-800">
         <h2 class="text-xl font-semibold text-gray-700 dark:text-white mb-4">Create Catalogs</h2>
         <p class="text-gray-500 dark:text-gray-300">Create catalogs for your products.</p>
         <button class="mt-4 inline-block bg-[#4f39f6] text-white px-6 py-2 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 hover:bg-[#3a2ac9]">
