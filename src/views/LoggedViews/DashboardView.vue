b3509349348e4db3377ff4658e235726b0253725
Cristhian <twisticdigital@gmail.com>
2025-05-02 13:41:10 +0100

    Modificar ruta a /newTemplate y texto descriptivo.

--- src/views/LoggedViews/DashboardView.vue
+++ src/views/LoggedViews/DashboardView.vue
@@ -59,9 +59,9 @@ onMounted(fetchNotifications);
       <!-- Create Catalogs Card (admin) -->
       <RouterLink to="/newCatalog" v-if="role && (role === 'admin')" class="p-6 bg-white rounded-2xl shadow-md hover:shadow-lg transition dark:bg-gray-800">
         <h2 class="text-xl font-semibold text-gray-700 dark:text-white mb-4">Create Catalogs</h2>
-        <p class="text-gray-500 dark:text-gray-300">Create catalogs for your products.</p>
+        <p class="text-gray-500 dark:text-gray-300">Create pdf catalogs for your products.</p>
         <button class="mt-4 inline-block bg-[#4f39f6] text-white px-6 py-2 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 hover:bg-[#3a2ac9]">
-          Create Catalogs
+          Create Pdf Catalog
         </button>
       </RouterLink>
 
