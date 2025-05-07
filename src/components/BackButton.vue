32fd4c00031e6a107d2a60cfe76574f50db2831c
Cristhian <twisticdigital@gmail.com>
2025-05-07 21:07:30 +0100

    Modificar estilos del boton back

--- src/components/BackButton.vue
+++ src/components/BackButton.vue
@@ -1,7 +1,7 @@
 <template>
   <button
     @click="goBack"
-    class="flex items-center text-sm text-gray-700 bg-gray-800 border border-gray-300 rounded-lg px-4 py-2 shadow hover:bg-gray-100 transition-all duration-200 ease-in-out transform hover:-translate-y-0.5 active:scale-95"
+    class="flex items-center text-sm text-gray-700 bg-gray-800 border border-gray-300 rounded-lg px-4 py-2 shadow transition-all duration-200 ease-in-out transform active:scale-95"
   >
     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
