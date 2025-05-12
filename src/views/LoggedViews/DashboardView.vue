576ec481ef4261d99aaf9322e4249516993cb443
Cristhian <twisticdigital@gmail.com>
2025-05-12 11:14:04 +0100

    Añadir imagen al card fieldsOperations

--- src/views/LoggedViews/DashboardView.vue
+++ src/views/LoggedViews/DashboardView.vue
@@ -176,13 +176,24 @@ onMounted(async () => {
 
 
       <!-- Fields Operations Card (admin) -->
-      <RouterLink to="/fields" v-if="role && (role === 'admin')" class="p-6 bg-white rounded-2xl shadow-md hover:shadow-lg transition dark:bg-gray-800">
-        <h2 class="text-xl font-semibold text-gray-700 dark:text-white mb-4">Fields Operations</h2>
-        <p class="text-gray-500 dark:text-gray-300">Create or delete fields for your templates.</p>
-        <button class="mt-4 inline-block bg-gray-800 text-white px-6 py-2 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 hover:bg-[#3a2ac9]">
-          Manage Fields
-        </button>
+      <RouterLink
+        to="/fields"
+        v-if="role && (role === 'admin')"
+        class="flex items-center justify-between p-6 bg-white rounded-2xl shadow-md hover:shadow-lg transition dark:bg-gray-800"
+      >
+        <!-- Contenido de texto -->
+        <div class="flex-1 pr-4">
+          <h2 class="text-xl font-semibold text-gray-700 dark:text-white mb-2">Fields Operations</h2>
+          <p class="text-gray-500 dark:text-gray-300 mb-4">Create or delete fields for your templates.</p>
+          <button class="bg-gray-800 text-white px-6 py-2 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 hover:bg-[#3a2ac9]">
+            Manage Fields
+          </button>
+        </div>
+
+        <!-- Imagen del icono -->
+        <img src="/public/fieldsOperations.png" alt="Fields Operations" class="w-24 h-24 object-contain">
       </RouterLink>
+
 
       <!-- Notifications Card (client and admin) -->
       <RouterLink to="notifications" v-if="role && (role === 'admin')" class="p-6 bg-white rounded-2xl shadow-md hover:shadow-lg transition dark:bg-gray-800">
