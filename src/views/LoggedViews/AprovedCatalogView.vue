8bb6d154c463e680dcf2d604f81cda703b48da97
Cristhian <twisticdigital@gmail.com>
2025-05-13 11:09:44 +0100

    Cambiar estilos del boton

--- src/views/LoggedViews/AprovedCatalogView.vue
+++ src/views/LoggedViews/AprovedCatalogView.vue
@@ -79,7 +79,7 @@ onMounted(async () => {
       >
         <h2 class="text-xl font-semibold text-gray-700 dark:text-white mb-4">{{ template.catalog_name }}</h2>
         <p class="text-gray-500 dark:text-gray-300 mb-4">Status: <span class="font-semibold text-yellow-500">{{ template.status }}</span></p>
-        <button class="mt-4 inline-block bg-[#4f39f6] text-white px-6 py-2 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 hover:bg-[#3a2ac9]">
+        <button class="bg-gray-800 text-white px-6 py-2 rounded-lg shadow-md transition-all duration-300 ease-in-out hover:px-8">
           Customize PDF
         </button>
       </RouterLink>
