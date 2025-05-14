0e3099c07d84ff17406f6a6ef54d0d35eccba00c
Cristhian <twisticdigital@gmail.com>
2025-05-14 11:18:34 +0100

    Colocar bien la imagen

--- src/views/LoggedViews/AprovedCatalogView.vue
+++ src/views/LoggedViews/AprovedCatalogView.vue
@@ -85,17 +85,22 @@ onMounted(async () => {
         v-for="template in approvedTemplates"
         :key="template.id_template"
         :to="`/customizePdf/${template.id_template}`"
-        class="p-6 bg-white rounded-2xl shadow-md hover:shadow-lg transition dark:bg-gray-800"
+        class="flex items-center justify-between p-6 bg-white rounded-2xl shadow-md hover:shadow-lg transition dark:bg-gray-800"
       >
-        <h2 class="text-xl font-semibold text-gray-700 dark:text-white mb-4">{{ template.catalog_name }}</h2>
-        <p class="text-sm text-gray-500 mb-2">Created: {{ formatDate(template.created_at) }}</p>
-        <p class="text-sm text-gray-500 mb-4">Updated: {{ formatDate(template.updated_at) }}</p>
-        <button class="bg-gray-800 text-white px-6 py-2 rounded-lg shadow-md transition-all duration-300 ease-in-out hover:px-8">
-          Customize PDF
-        </button>
+        <!-- Contenido de texto -->
+        <div class="flex-1 pr-4">
+          <h2 class="text-xl font-semibold text-gray-700 dark:text-white mb-2">{{ template.catalog_name }}</h2>
+          <p class="text-sm text-gray-500 mb-1">Creado: {{ formatDate(template.created_at) }}</p>
+          <p class="text-sm text-gray-500 mb-4">Actualizado: {{ formatDate(template.updated_at) }}</p>
+          <button class="bg-gray-800 text-white px-6 py-2 rounded-lg shadow-md transition-all duration-300 ease-in-out hover:px-8">
+            Customize PDF
+          </button>
+        </div>
+
         <!-- Imagen -->
-        <img src="/public/viewCatalog.png" alt="View Catalogs" class="w-24 h-24 object-contain">
+        <img src="/viewCatalog.png" alt="View Catalog" class="w-24 h-24 object-contain" />
       </RouterLink>
+
     </div>
     <div class="mt-12">
       <BackButton
