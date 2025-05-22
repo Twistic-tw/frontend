cbc597e3d7a88ed1ad181a1dc32649747ff475f3
Cristhian <twisticdigital@gmail.com>
2025-05-22 10:08:20 +0100

    .

--- src/views/LoggedViews/CustomizePdfView.vue
+++ src/views/LoggedViews/CustomizePdfView.vue
@@ -88,34 +88,6 @@ const {
           </div>
         </template>
       </draggable>
-      <!-- Text Customization Card -->
-        <div class="bg-white rounded-2xl shadow-lg p-6 border border-gray-200">
-          <h2 class="text-xl font-semibold text-gray-800 mb-3">Text Options</h2>
-          <div class="flex flex-col gap-4">
-            <div>
-              <label class="block text-sm font-medium text-gray-700 mb-1">Header Text</label>
-              <input type="color" v-model="colors.headerText" class="w-full h-8 border rounded" />
-            </div>
-            <div>
-              <label class="block text-sm font-medium text-gray-700 mb-1">Content Text</label>
-              <input type="color" v-model="colors.text" class="w-full h-8 border rounded" />
-            </div>
-            <div>
-              <label class="block text-sm font-medium text-gray-700 mb-1">Footer Text</label>
-              <input type="color" v-model="colors.footerText" class="w-full h-8 border rounded" />
-            </div>
-            <div>
-              <label class="block text-sm font-medium text-gray-700 mb-1">Field Font</label>
-              <select v-model="titleSettings.fieldFont" class="w-full p-2 border rounded">
-                <option value="Arial">Arial</option>
-                <option value="Helvetica">Helvetica</option>
-                <option value="Times New Roman">Times New Roman</option>
-                <option value="Georgia">Georgia</option>
-                <option value="Courier New">Courier New</option>
-              </select>
-            </div>
-          </div>
-        </div>
     </div>
 
     <!-- Estilos e Imágenes -->
@@ -144,7 +116,6 @@ const {
             <input type="color" v-model="colors.headerColor" class="w-full h-8 border rounded mb-1" />
             <input type="range" min="0" max="1" step="0.01" v-model.number="colors.headerAlpha" class="w-full bg-[#1e2939]"/>
           </div>
-          <!--
           <div>
             <label class="block text-sm font-medium text-gray-700 mb-1">Header Text</label>
             <input type="color" v-model="colors.headerText" class="w-full h-8 border rounded" />
@@ -181,9 +152,7 @@ const {
               <option value="Courier New">Courier New</option>
             </select>
           </div>
-          -->
         </div>
-
       </div>
 
       <!-- Imágenes -->
