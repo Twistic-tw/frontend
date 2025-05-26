df5ed75e340440df3880ffd128131214a13edb94
Cristhian <twisticdigital@gmail.com>
2025-05-26 12:39:03 +0100

    Boton de cerrar en la parte inferior central

--- src/components/LivePreviewModal.vue
+++ src/components/LivePreviewModal.vue
@@ -14,15 +14,6 @@
             &times;
           </button>
 
-          <div class="flex justify-end mb-4">
-            <button
-              @click="$emit('close')"
-              class="bg-gray-800 text-white text-sm px-5 py-2 rounded-lg shadow hover:bg-gray-900 transition"
-            >
-              Cerrar vista ampliada
-            </button>
-          </div>
-
           <div class="scale-100 origin-top-left">
             <div class="bg-white shadow rounded-lg p-6">
               <!-- Vista renderizada directamente -->
@@ -59,6 +50,19 @@
                 </div>
               </div>
             </div>
+          </div>
+
+          <!-- Botón flotante abajo -->
+          <div class="fixed bottom-6 left-1/2 transform -translate-x-1/2 z-50">
+            <button
+              @click="$emit('close')"
+              class="bg-gray-800 text-white p-3 rounded-full shadow-lg hover:bg-gray-900 transition"
+              title="Cerrar"
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </button>
           </div>
         </div>
       </div>
