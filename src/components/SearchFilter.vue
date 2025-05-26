1fc0d2805ee8ad95f8cb4d559eefd314c722a8f8
Cristhian <twisticdigital@gmail.com>
2025-05-26 13:36:20 +0100

    Modificar boton cerrar

--- src/components/SearchFilter.vue
+++ src/components/SearchFilter.vue
@@ -18,9 +18,19 @@
           <!-- Botón cerrar -->
           <button
             @click="showModal = false"
-            class="absolute top-3 right-3 text-gray-400 hover:text-gray-800 text-2xl font-bold"
+            class="absolute top-4 right-4 text-gray-400 hover:text-red-500 transition-colors duration-300"
+            aria-label="Close"
           >
-            &times;
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+            </svg>
           </button>
 
           <!-- Filtros -->
