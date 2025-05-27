9d98e6f4bd1bf2204f35ce27d6fd66a6f70be42b
Cristhian <twisticdigital@gmail.com>
2025-05-27 14:11:02 +0100

    Correciones

--- src/components/TextOptions.vue
+++ src/components/TextOptions.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-white rounded-2xl shadow-lg border border-gray-200 p-6 cursor-pointer hover:bg-gray-50 transition" @click="$emit('toggle', cardId)">
-    <h2 class="text-xl font-semibold text-gray-800">Text Options</h2>
+    <h2 class="text-xl font-bold text-gray-800">Text Options</h2>
   </div>
 </template>
 
