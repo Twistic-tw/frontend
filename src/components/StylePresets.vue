103ec703ef559b786b04c06ca011823f2244a12c
Cristhian <twisticdigital@gmail.com>
2025-05-29 11:53:48 +0100

    Correciones

--- src/components/StylePresets.vue
+++ src/components/StylePresets.vue
@@ -1,8 +1,9 @@
 <template>
   <div
     class="bg-white p-4 rounded-2xl shadow-md mb-6 cursor-pointer hover:shadow-lg transition"
-    @click="openModal"
+    @click="() => { if (!showStyleModal) openModal() }"
   >
+
     <h2 class="text-xl font-bold text-gray-800">
       {{ $t('style_presets_title') }}
     </h2>
