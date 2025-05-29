cfff93e3dbe4bcbc1dca39040706492dcb4bf341
Cristhian <twisticdigital@gmail.com>
2025-05-29 11:07:42 +0100

    Modificar h2

--- src/components/StylePresets.vue
+++ src/components/StylePresets.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-white p-4 rounded-2xl shadow-md mb-6 cursor-pointer hover:shadow-lg transition" @click="openModal = true">
-    <h2 class="text-lg font-bold text-gray-800">{{ $t('style_presets_title') }}</h2>
+    <h2 class="text-xl font-bold text-gray-800">{{ $t('style_presets_title') }}</h2>
 
     <!-- Modal -->
     <StylePresetsModal
