30567d0c45ce5fe3c77a16201c9ae1ada582a049
Cristhian <twisticdigital@gmail.com>
2025-06-03 13:02:10 +0100

    .

--- src/components/BooleanFilter.vue
+++ src/components/BooleanFilter.vue
@@ -7,7 +7,7 @@
 
     <label class="block text-sm font-medium text-gray-700 mb-1">{{ $t('select_boolean_value') }}</label>
     <select v-model="selected" @change="emitFilter" class="w-full p-2 border rounded shadow-sm">
-      <option disabled value="">-- {{ $t('select_option') }} --</option>
+      <option disabled value="">{{ $t('select_option') }}</option>
       <option value="true">{{ $t('yes') }}</option>
       <option value="false">{{ $t('no') }}</option>
     </select>
