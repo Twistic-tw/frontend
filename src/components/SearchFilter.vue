ae4e8cc4e44bab41f84efe4ff61caf97ee06d8f4
Cristhian <twisticdigital@gmail.com>
2025-06-04 17:04:57 +0100

    .

--- src/components/SearchFilter.vue
+++ src/components/SearchFilter.vue
@@ -14,7 +14,7 @@
         @click.self="showModal = false"
       >
         <div class="bg-white w-[95%] h-[90vh] rounded-xl shadow-xl overflow-hidden relative flex">
-          <!-- Botón de cerrar -->
+          <!-- Botón de cerrar
           <button
             @click="showModal = false"
             class="absolute top-4 right-4 z-30 bg-gray-800 text-white hover:bg-red-600 hover:scale-105 transition-all duration-300 rounded-full w-10 h-10 flex items-center justify-center shadow-md"
@@ -23,7 +23,7 @@
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
               <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
             </svg>
-          </button>
+          </button>-->
 
           <!-- Panel lateral de filtros -->
           <aside class="w-full md:w-1/3 lg:w-1/4 h-full overflow-y-auto border-r p-6 space-y-6 bg-gray-50">
