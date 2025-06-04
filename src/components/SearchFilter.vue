95a60e5a06d631e1abeeafcf59cd9c034b24122b
Cristhian <twisticdigital@gmail.com>
2025-06-04 11:54:42 +0100

    .

--- src/components/SearchFilter.vue
+++ src/components/SearchFilter.vue
@@ -40,7 +40,6 @@
                 </option>
               </select>
             </div>
-            <div v-if="searchField">
               <transition name="fade-slide">
                 <div v-if="searchField" class="transition-all duration-300 ease-in-out">
                   <component
@@ -57,13 +56,6 @@
                   </button>
                 </div>
               </transition>
-              <button
-                @click="$emit('clear')"
-                class="w-full mt-4 bg-gray-300 text-gray-800 px-4 py-2 rounded hover:bg-gray-400 transition shadow-sm"
-              >
-                {{ t('clear') }}
-              </button>
-            </div>
           </aside>
 
           <!-- Tabla -->
