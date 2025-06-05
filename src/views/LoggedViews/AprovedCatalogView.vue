181ba095c26a49e0acfe1a1f36ccf6a879b284fb
Cristhian <twisticdigital@gmail.com>
2025-06-05 12:54:22 +0100

    .

--- src/views/LoggedViews/AprovedCatalogView.vue
+++ src/views/LoggedViews/AprovedCatalogView.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
-console.log('Vista createCatalogues montada correctamente')
+import { ApprovedCatalog } from '../../composable/ApprovedCatalog'
+
+console.log('Vista createCatalogues montada con composable cargado')
+ApprovedCatalog()
 </script>
 
 <template>
   <div class="p-6">
-    <h1 class="text-2xl font-bold">Vista de prueba: Create Catalogues</h1>
+    <h1 class="text-2xl font-bold">Create Catalogues con composable</h1>
     <router-link to="/dashboard" class="text-blue-500 underline">Ir a Dashboard</router-link>
   </div>
 </template>
