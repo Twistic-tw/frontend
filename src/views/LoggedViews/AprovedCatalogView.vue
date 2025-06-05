912547472f815dd8a56ee361927abb21a513e2c0
Cristhian <twisticdigital@gmail.com>
2025-06-05 12:55:15 +0100

    .

--- src/views/LoggedViews/AprovedCatalogView.vue
+++ src/views/LoggedViews/AprovedCatalogView.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import { ApprovedCatalog } from '../../composable/ApprovedCatalog'
 
-console.log('Vista createCatalogues montada con composable cargado')
-ApprovedCatalog()
+const { approvedTemplates } = ApprovedCatalog()
 </script>
 
 <template>
   <div class="p-6">
-    <h1 class="text-2xl font-bold">Create Catalogues con composable</h1>
-    <router-link to="/dashboard" class="text-blue-500 underline">Ir a Dashboard</router-link>
+    <h1 class="text-2xl font-bold">Render simple de templates</h1>
+    <div v-for="t in approvedTemplates" :key="t.id_template" class="p-2 border-b border-gray-300">
+      {{ t.catalog_name }}
+    </div>
+    <router-link to="/dashboard" class="text-blue-500 underline mt-4 block">Ir a Dashboard</router-link>
   </div>
 </template>
