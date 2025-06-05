f72c5ff4370876b2719dce7b77fe5429741ec248
Cristhian <twisticdigital@gmail.com>
2025-06-05 10:55:43 +0100

    Eliminar padding al footer

--- src/components/LivePreviewModal.vue
+++ src/components/LivePreviewModal.vue
@@ -97,7 +97,7 @@
             </div>
 
             <!-- Página extra solo con footer -->
-            <div v-if="footerUrl" class="a4-page">
+            <div v-if="footerUrl" class="a4-page p-0">
               <div class="a4-image full-a4">
                 <img :src="footerUrl" alt="Footer Image" class="a4-image-content no-radius" />
               </div>
