4735c86b81b1357989a95402b1c2058b85f08bf4
Cristhian <twisticdigital@gmail.com>
2025-06-09 13:09:35 +0100

    .

--- src/components/LivePreviewModal.vue
+++ src/components/LivePreviewModal.vue
@@ -70,7 +70,7 @@
 
               <!-- Descripción corta -->
               <div v-if="descriptions.short" class="short-description">
-                <p :style="{ color: (typeof shortDescriptionStyle === 'string' ? shortDescriptionStyle : '#000') }">{{ descriptions.short || "Vacio"}}</p>
+                <p :style="{ color: shortDescriptionColor || '#000'}">{{ descriptions.short || "Vacio"}}</p>
               </div>
 
               <!-- Contenedor con tabla a la izquierda y imágenes a la derecha -->
@@ -132,10 +132,7 @@
 
               <!-- Descripción larga -->
               <div v-if="descriptions.long" class="long-description">
-                <p :style="{
-                   color: typeof longDescriptionStyle === 'string' ? longDescriptionStyle : 'white',
-
-                   }">{{ descriptions.long  || "Vacio"}}</p>
+                <p :style="{ color: longDescriptionColor || '#000'}">{{ descriptions.long  || "Vacio"}}</p>
               </div>
 
               <!-- Footer y número de página -->
@@ -184,8 +181,6 @@ const props = defineProps<{
   activeFieldNames: string[],
   headerStyle: Record<string, string>,
   templateName: string,
-  shortDescriptionStyle: Record<string, string>
-  longDescriptionStyle: Record<string, string>
   footerStyle: Record<string, string>,
   rowsPerPage: number,
   rowStyle: (index: number) => Record<string, string>,
