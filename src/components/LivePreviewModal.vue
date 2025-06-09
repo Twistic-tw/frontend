dd24e6d382b82d1c5d9f5b99d051de3bcfb9185c
Cristhian <twisticdigital@gmail.com>
2025-06-09 12:15:18 +0100

    .

--- src/components/LivePreviewModal.vue
+++ src/components/LivePreviewModal.vue
@@ -70,7 +70,7 @@
 
               <!-- Descripción corta -->
               <div v-if="descriptions.short" class="short-description">
-                <p>{{ descriptions.short }}</p>
+                <p :style="{ color: shortDescriptionColor || '#000' }">{{ descriptions.short || "Vacio"}}</p>
               </div>
 
               <!-- Contenedor con tabla a la izquierda y imágenes a la derecha -->
@@ -132,12 +132,12 @@
 
               <!-- Descripción larga -->
               <div v-if="descriptions.long" class="long-description">
-                <p style="white-space: pre-wrap">{{ descriptions.long }}</p>
+                <p :style="{ color: longDescriptionColor || '#000' }">{{ descriptions.long  || "Vacio"}}</p>
               </div>
 
               <!-- Footer y número de página -->
               <div class="footer-bar" :style="footerStyle">
-                <div class="footer-text">{{ descriptions.footer }}</div>
+                <div class="footer-text">{{ descriptions.footer  || "Vacio"}}</div>
                 <div class="page-number">{{ pageIndex + 1 }}</div>
               </div>
             </div>
