bb7560d49dca6de5d5c407413bfced9fd87dbe53
Cristhian <twisticdigital@gmail.com>
2025-06-09 12:57:03 +0100

    .

--- src/components/LivePreviewModal.vue
+++ src/components/LivePreviewModal.vue
@@ -70,7 +70,7 @@
 
               <!-- Descripción corta -->
               <div v-if="descriptions.short" class="short-description">
-                <p :style="{ color: shortDescriptionColor || '#000' }">{{ descriptions.short || "Vacio"}}</p>
+                <p :style="{ color: (typeof shortDescriptionStyle === 'string' ? shortDescriptionStyle : '#000') }">{{ descriptions.short || "Vacio"}}</p>
               </div>
 
               <!-- Contenedor con tabla a la izquierda y imágenes a la derecha -->
@@ -132,7 +132,7 @@
 
               <!-- Descripción larga -->
               <div v-if="descriptions.long" class="long-description">
-                <p :style="{ color: longDescriptionColor || '#000' }">{{ descriptions.long  || "Vacio"}}</p>
+                <p :style="{ color: typeof longDescriptionStyle === 'string' ? longDescriptionStyle : '#000' }">{{ descriptions.long  || "Vacio"}}</p>
               </div>
 
               <!-- Footer y número de página -->
